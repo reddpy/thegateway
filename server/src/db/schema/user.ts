45c8b5c6b33ff393bebabf1ee7ef92c5d036b1db
KJ <19541224+reddpy@users.noreply.github.com>
2025-04-27 15:03:02 -0400

    set up drizzle and command tools

---
+++ server/src/db/schema/user.ts
@@ -0,0 +1,8 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  first_name: varchar({ length: 50 }).notNull(),
+  last_name: varchar({ length: 50 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
