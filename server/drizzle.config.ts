3e34c7af38135bd50a98c7ebecba23496ab817d0
KJ <19541224+reddpy@users.noreply.github.com>
2025-04-28 15:07:14 -0400

    integrated postgres neon for deployment

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,6 +1,11 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+
+if(!process.env.DATABASE_URL){
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema',
