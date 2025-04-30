a463ec3e0de8ea7f25a3a3105031568b4010a69a
KJ <19541224+reddpy@users.noreply.github.com>
2025-04-30 15:42:36 -0400

    terraformed backend to handle better-auth and changed server env configuration with docker

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from "hono";
+import { auth } from "./lib/auth";
 
 const app = new Hono();
+
+app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 
 app.get("/", (c) => {
   return c.text("Hello Hono");
