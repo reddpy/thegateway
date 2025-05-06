24cc27a707505f16c1145ab78ba66fea71ed4bb7
KJ <19541224+reddpy@users.noreply.github.com>
2025-05-05 20:34:12 -0400

    created agent details layout and tabs

---
+++ client-web/src/app/dashboard/agent/[id]/details/layout.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { Cable, FileUp } from "lucide-react";
+
+export default function AgentDetailsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="container mx-auto py-1">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
+        <div className="px-0 md:px-4 w-full md:max-w-[60%]">
+          <h1 className="font-bold text-xl sm:text-2xl">
+            PLACEHOLDER FOR AGENT NAME: Customer Support Agent
+          </h1>
+          <p className="text-gray-600 text-sm sm:text-base">
+            PLACEHOLDER FOR AGENT DESCRIPTION: Handles customer inquiries and
+            ticket management
+          </p>
+        </div>
+
+        <div className="flex flex-wrap w-full md:w-auto gap-3 shadow-md border-2 border-indigo-300 px-2 py-2 rounded-xl">
+          <Button
+            variant="outline"
+            className="flex-1 md:flex-auto min-w-[140px] h-auto py-2 text-sm sm:text-md text-gray-500 hover:text-black"
+          >
+            <Cable className="mr-2" />
+            <span>
+              Connect <br className="hidden sm:block" /> Github
+            </span>
+          </Button>
+          <Button
+            variant="outline"
+            className="flex-1 md:flex-auto min-w-[140px] h-auto py-2 text-sm sm:text-md text-gray-500 hover:text-black"
+          >
+            <FileUp className="mr-2" />
+            <span>
+              Upload <br className="hidden sm:block" /> Code
+            </span>
+          </Button>
+        </div>
+      </div>
+      <hr className="mt-4" />
+      <div className="mt-2">{children}</div>
+    </div>
+  );
+}
--- client-web/src/app/dashboard/agent/[id]/details/page.tsx
+++ client-web/src/app/dashboard/agent/[id]/details/page.tsx
@@ -1,42 +1,62 @@
-import { Button } from "@/components/ui/button";
-import { FileUp, Plug } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Database, EarthLock, Lock, Plug, Target, Wrench } from "lucide-react";
 
-export default function Page() {
+export default async function Page() {
   return (
-    <div className="container mx-auto px-2 py-1">
-      <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
-        <div className="px-0 md:px-4 w-full md:max-w-[60%]">
-          <h1 className="font-bold text-xl sm:text-2xl">
-            PLACEHOLDER FOR AGENT NAME: Customer Support Agent
-          </h1>
-          <p className="text-gray-600 text-sm sm:text-base">
-            PLACEHOLDER FOR AGENT DESCRIPTION: Handles customer inquiries and
-            ticket management
-          </p>
-        </div>
+    <Tabs defaultValue="overview" className="w-full">
+      <TabsList className="border-b w-full justify-start h-12">
+        <TabsTrigger
+          value="overview"
+          className="data-[state=active]:border-indigo-400 data-[state=active]:shadow-sm"
+        >
+          <Target color="green" />
+          <p>Overview</p>
+        </TabsTrigger>
+        <TabsTrigger
+          value="security"
+          className="data-[state=active]:border-indigo-400 data-[state=active]:shadow-sm"
+        >
+          <EarthLock color="blue" />
+          <p>Security</p>
+        </TabsTrigger>
+        <TabsTrigger
+          value="data-sources"
+          className="data-[state=active]:border-indigo-400 data-[state=active]:shadow-sm"
+        >
+          <Database color="red" />
+          <p>Data Sources</p>
+        </TabsTrigger>
+        <TabsTrigger
+          value="api-interfaces"
+          className="data-[state=active]:border-indigo-400 data-[state=active]:shadow-sm"
+        >
+          <Plug color="purple" />
+          <p>API Interfaces</p>
+        </TabsTrigger>
+        <TabsTrigger
+          value="permissions"
+          className="data-[state=active]:border-indigo-400 data-[state=active]:shadow-sm"
+        >
+          <Lock color="orange" />
+          <p>Permissions</p>
+        </TabsTrigger>
+        <TabsTrigger
+          value="settings"
+          className="data-[state=active]:border-indigo-400 data-[state=active]:shadow-sm"
+        >
+          <Wrench />
+          <p>Settings</p>
+        </TabsTrigger>
+      </TabsList>
 
-        <div className="flex flex-wrap w-full md:w-auto gap-3 shadow-md border-2 border-indigo-300 px-2 py-2 rounded-xl">
-          <Button
-            variant="outline"
-            className="flex-1 md:flex-auto min-w-[140px] h-auto py-2 text-sm sm:text-md text-gray-500 hover:text-black"
-          >
-            <Plug className="mr-2"/>
-            <span>
-              Connect <br className="hidden sm:block" /> Github
-            </span>
-          </Button>
-          <Button
-            variant="outline"
-            className="flex-1 md:flex-auto min-w-[140px] h-auto py-2 text-sm sm:text-md text-gray-500 hover:text-black"
-          >
-            <FileUp className="mr-2" />
-            <span>
-              Upload <br className="hidden sm:block" /> Code
-            </span>
-          </Button>
-        </div>
-      </div>
-      <hr className="mt-4" />
-    </div>
+      <TabsContent value="overview">Overview content here</TabsContent>
+      <TabsContent value="security">Security content here</TabsContent>
+      <TabsContent value="data-sources">Data Sources content here</TabsContent>
+      <TabsContent value="api-interfaces">
+        API Interfaces content here
+      </TabsContent>
+      <TabsContent value="permissions">Permissions content here</TabsContent>
+      <TabsContent value="settings">Settings content here</TabsContent>
+    </Tabs>
   );
 }
